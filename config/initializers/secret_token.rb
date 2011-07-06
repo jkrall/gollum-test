3c635cb1b94fc29fcadc0fe6bd9a5c1565e9a587
Joshua Krall <joshuakrall@pobox.com>
2011-07-06 15:51:04 -0500

    Adding rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GollumTest::Application.config.secret_token = 'c635793b40c53e8536b0ddf2892d3e92c308fe700da95689c17ffc11d26767db856b9fab56ec269ee58fee1e9f47d59f716ffc2800fd1c9326e0c3bd8c53736c'
